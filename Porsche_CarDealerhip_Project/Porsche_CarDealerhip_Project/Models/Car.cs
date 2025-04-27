a599b8d1a774933aaa5c564febc8c9dea0cc54c1
jcssll <essentialnetworker@gmail.com>
2025-04-27 18:35:58 -0400

    Create Car.cs
    created blueprint for cars that will connect to a list of car options

---
+++ Porsche_CarDealerhip_Project/Porsche_CarDealerhip_Project/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace Porsche_CarDealerhip_Project.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public int Mileage { get; set; }
+        public decimal Price { get; set; }
+        public string? ImagePath { get; set; }
+
+        //public ICollection<CarOption> CarOptions { get; set; } = new List<CarOption>();
+    }
+}
