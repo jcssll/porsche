fecc83fb0003c9fbd30e9c8fb0c1ba17eaaf31f8
jcssll <essentialnetworker@gmail.com>
2025-04-28 12:30:06 -0400

    Create CarOption.cs
    Created CarOption model with properties

---
+++ Porsche_CarDealerhip_Project/Porsche_CarDealerhip_Project/Models/CarOption.cs
@@ -0,0 +1,13 @@
+﻿namespace Porsche_CarDealerhip_Project.Models
+{
+    public class CarOption
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int OptionId { get; set; }
+
+        public Car Car { get; set; } = null!;
+
+        public Option Option { get; set; } = null!;
+    }
+}
