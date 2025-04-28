9ce9289a44d3b9d37fc93774fd502233a3ed335f
jcssll <essentialnetworker@gmail.com>
2025-04-28 12:30:44 -0400

    Create Option.cs
    Added Option model with properties and connected it to the ICollection

---
+++ Porsche_CarDealerhip_Project/Porsche_CarDealerhip_Project/Models/Option.cs
@@ -0,0 +1,10 @@
+﻿namespace Porsche_CarDealerhip_Project.Models
+{
+    public class Option
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<CarOption> CarOptions { get; set; } = new List<CarOption>();
+    }
+}
