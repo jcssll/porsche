6f0d0642209b51f1681b28a0aa00aef331af422c
jcssll <essentialnetworker@gmail.com>
2025-04-28 12:34:49 -0400

    Update Program.cs
    - Added DbContext registration in Program.cs
    - Configured Entity Framework Core to use SQL Server
    - Pulled connection string from appsettings.json ("DefaultConnection")
    - Enabled dependency injection for PorscheDbContext throughout the application

--- Porsche_CarDealerhip_Project/Porsche_CarDealerhip_Project/Program.cs
+++ Porsche_CarDealerhip_Project/Porsche_CarDealerhip_Project/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Porsche_CarDealerhip_Project.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+//  
+builder.Services.AddDbContext<PorscheDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
