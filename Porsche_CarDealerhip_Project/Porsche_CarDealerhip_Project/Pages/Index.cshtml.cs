621fa84a3d69d6251cf1d11957a059c0510875aa
jcssll <essentialnetworker@gmail.com>
2025-04-28 12:33:59 -0400

    Update Index.cshtml.cs
    Add database connection to page
    
    Load cars and options dynamically
    
    Allow filtering by selected car options
    
    Prepare data for dynamic rendering in Razor view (Index.cshtml

--- Porsche_CarDealerhip_Project/Porsche_CarDealerhip_Project/Pages/Index.cshtml.cs
+++ Porsche_CarDealerhip_Project/Porsche_CarDealerhip_Project/Pages/Index.cshtml.cs
@@ -1,19 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Porsche_CarDealerhip_Project.Data;
+using Porsche_CarDealerhip_Project.Models;
 
 namespace Porsche_CarDealerhip_Project.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly PorscheDbContext _context;
+
+
+
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(PorscheDbContext context, ILogger<IndexModel> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
-    public void OnGet()
+    [BindProperty(SupportsGet = true)]
+    public List<string> SelectedOptions { get; set; } = new List<string>();
+
+    public List<Car> Cars { get; set; } = new List<Car>();
+
+    public List<Option> AvailableOptions { get; set; } = new List<Option>();
+
+    public async Task OnGetAsync()
     {
+        AvailableOptions = await _context.Options.ToListAsync();
+
+        var query = _context.Cars
+            .Include(c => c.CarOptions)
+            .ThenInclude(co => co.Option)
+            .AsQueryable();
+        if (SelectedOptions != null && SelectedOptions.Any())
+        {
+            query = query.Where(c => c.CarOptions
+                .Any(co => SelectedOptions.Contains(co.Option.Name)));
+        }
+
+        Cars = await query.ToListAsync();
 
     }
 }
