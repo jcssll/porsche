232ac416f96d7374994691b014dd537f663baf8f
jcssll <essentialnetworker@gmail.com>
2025-04-28 12:27:38 -0400

    Create PorscheDbContext.cs
    Add PorscheDbContext with DbSets for Car, Option, and CarOption. Configure relationships between CarOption → Car and CarOption → Option.

---
+++ Porsche_CarDealerhip_Project/Porsche_CarDealerhip_Project/Data/PorscheDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Porsche_CarDealerhip_Project.Models;
+
+
+namespace Porsche_CarDealerhip_Project.Data
+{
+    public class PorscheDbContext : DbContext
+    {
+        public PorscheDbContext(DbContextOptions<PorscheDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<CarOption> CarOptions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CarOption>()
+                .HasOne(co => co.Car)
+                .WithMany(c => c.CarOptions)
+                .HasForeignKey(co => co.CarId);
+
+            modelBuilder.Entity<CarOption>()
+                .HasOne(co => co.Option)
+                .WithMany(o => o.CarOptions)
+                .HasForeignKey(co => co.OptionId);
+        }
+
+
+    }
+}
